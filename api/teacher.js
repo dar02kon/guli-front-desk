aa770669297e318b8f5812cb5df35fbb7e55830d
daring02 <1482882482@qq.com>
2022-10-20 17:10:11 +0800

    谷粒学苑前台网页

---
+++ api/teacher.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request'
+const api_name = '/eduservice/teacherfront'
+export default {
+  getPageList(page, limit) {   
+    return request({
+      url: `${api_name}/${page}/${limit}`,
+      method: 'get'
+    })
+  },
+
+  
+getById(teacherId) {
+        return request({
+            url: `${api_name}/${teacherId}`,
+            method: 'get'
+        })
+    }
+}
